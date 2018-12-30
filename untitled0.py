b2d9f36107f8bf658f61d7936bee3a54c0310908
sam2702 <Singh.santosh2702@gmail.com>
2018-12-30 18:19:46 +0530

    new

--- untitled0.py
+++ untitled0.py
@@ -4,9 +4,9 @@ Created on Tue Dec 25 16:38:54 2018
 
 @author: D3XT3R
 """
- Item based Collaborative Filtering is an algorithm based on the similarity between 
- items so that they can provide suggestions to users.
- Step 1: Read the data and create the input matrix 
+ #Item based Collaborative Filtering is an algorithm based on the similarity between 
+ #items so that they can provide suggestions to users.
+ #Step 1: Read the data and create the input matrix 
 
 
 import numpy as np
@@ -23,15 +23,15 @@ print()
 print(data)
 
 
-Step 2: Calculate similarities between movies
-The similarity here is calculated based on the cosine distance:
+#Step 2: Calculate similarities between movies
+#The similarity here is calculated based on the cosine distance:
    # Suppose we calculate the similarities of the two films m1 and m2,we find that these two films are evaluated by the user u2 and u3 (as in the data above).
    # We will create 2 item-vector v1 for m1 and v2 for m2 in user-space (u2, u3).
-   v1 = 5 u2 + 3 u3 
-   v2 = 2 u2 + 3 u3
+   #v1 = 5 u2 + 3 u3 
+   #v2 = 2 u2 + 3 u3
    
    # The cosine similarity is then calculated by the formula:
-   cos(v1, v2) = (53 + 23) / sqrt[(25 + 9) * (4 + 9)] = 0.9037
+   #cos(v1, v2) = (53 + 23) / sqrt[(25 + 9) * (4 + 9)] = 0.9037
    
    # Similarly, repeating with each pair of different movies, we can calculate the similarities between them. The result will be the similarity matrix between the movies.
    
@@ -59,3 +59,34 @@ for m in range(n_movie):
 print("Similarity matrix between movies:")
 print(similarity_matrix)
 
+
+
+def cal_rating(user_rating_vector, target_movie):
+    upper = 0.
+    lower = 0.
+    for m in range(n_movie):
+        if user_rating_vector[m] == -1 or m == target_movie: continue
+        upper += user_rating_vector[m] * similarity_matrix[target_movie, m]
+        lower += similarity_matrix[target_movie, m]
+    rating = upper/lower
+    return rating
+    
+unrated_movie = np.zeros((n_user, n_movie), dtype=np.float32)
+
+# For each user and for each movie that user hadn't rated yet, predict rating for that user - movie pair
+for u in range(n_user):
+    for m in range(n_movie):
+        if data[u, m] == -1:
+            user_ratings = data[u, :]
+            unrated_movie[u, m] = cal_rating(user_ratings, m)
+print("Original data:")
+print(data)
+print()
+print("Predict rating only data:")
+print(unrated_movie)
+
+
+# Find the max rating
+recommend_movie = unrated_movie.argmax(axis=1)
+for user, movie in enumerate(recommend_movie): 
+    print("Recommend movie {} for user {}".format(movie, user))
